f67f29df95465c96f502f869fe01ea7880dc251a
arkhaminferno <gauravpsp@gmail.com>
2025-12-17 20:10:11 +0530

    feat: update react + next

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,14 +1,29 @@
 /// <reference types="vite/client" />
 
 // Declare iconify-icon custom element for TypeScript
-declare namespace JSX {
-  interface IntrinsicElements {
-    'iconify-icon': React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement> & {
-        icon?: string;
-        style?: React.CSSProperties;
-      },
-      HTMLElement
-    >;
+import type * as React from 'react';
+
+type IconifyIconElementProps = React.DetailedHTMLProps<
+  React.HTMLAttributes<HTMLElement> & {
+    icon?: string;
+    style?: React.CSSProperties;
+  },
+  HTMLElement
+>;
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'iconify-icon': IconifyIconElementProps;
+    }
+  }
+}
+
+// Some TS/React setups read intrinsic elements from the React module typings.
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements {
+      'iconify-icon': IconifyIconElementProps;
+    }
   }
 }
